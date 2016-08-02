2a3417530f286d5835693d0c992e12105bc34906
Cesar Valdez <cesar.valdez@feedhenry.com>
2016-08-02 10:18:07 +0100

    fixing ios and adding to libraries to Android

--- www/pdf.js
+++ www/pdf.js
@@ -2,15 +2,15 @@
 
 module.exports = {
     htmlToPDF: function (options, successCallback, errorCallback) {
-         
+
          if(!options.url && !options.data) throw "No URL or HTML Data found.";
-         
+
          var url = options.url;
          var data = options.data;
          var docSize = options.documentSize || "A4";
          var landscape = options.landscape || "portrait";
          var type = options.type || "base64";
-        
-        cordova.exec(successCallback, errorCallback, "PDFService", "htmlToPDF", [ url, data, docSize, landscape, type ]);
+
+        cordova.exec(successCallback, errorCallback, "PDFGenerator", "htmlToPDF", [ url, data, docSize, landscape, type ]);
     }
 };
